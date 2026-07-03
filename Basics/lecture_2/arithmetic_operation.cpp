8f30ccc5074722b11589c68302d1c29a81218e31
Suraj Kumar <surajkm898@gmail.com>
2026-07-03 22:03:10 +0530

    update

---
+++ Basics/lecture_2/arithmetic_operation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n = 8 , y = 6 ;
+    cout << n + y << endl ;
+    cout << n - y << endl ;
+    cout << n * y << endl ;
+    cout << n % y << endl ;
+
+}
