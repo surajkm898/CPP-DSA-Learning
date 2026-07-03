396d97c7ab26ebc2ba4ae57ab638c5949359d682
Suraj Kumar <surajkm898@gmail.com>
2026-07-03 22:22:33 +0530

    update

---
+++ cal_volume_of_sphere.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int r ;
+    cout << " enter your radi :";
+    cin >> r ;
+    float pi= 3.14;
+    float m = 1.33;
+    cout << pi * r * r * m * r ;
+}
