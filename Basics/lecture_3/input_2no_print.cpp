181dc823c27b213f44b5ad834ee3dc7b43679224
Suraj Kumar <surajkm898@gmail.com>
2026-07-07 21:02:20 +0530

    update

---
+++ Basics/lecture_3/input_2no_print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n , x;
+    cout << "enter your 1 number";
+    cin >> n;
+    cout << "enter your 2 number";
+    cin >> x;
+    cout << n + x ;
+}
