6f13945c901dac70223f80cf45ecf7aa473d6cb8
Suraj Kumar <surajkm898@gmail.com>
2026-07-07 21:34:35 +0530

    update

---
+++ if-else_02/lecture_4/can_drive_or_not.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << " enter your age" ;
+    cin >> n ;
+    if (n >= 18) cout << " can drive" ;
+    else cout << " not drive ";
+}
