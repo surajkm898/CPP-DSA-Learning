425dac18f66c6703788ebd3d293cdfbea6cd9708
Suraj Kumar <surajkm898@gmail.com>
2026-07-08 23:23:47 +0530

    update

---
+++ if-else_02/lecture_4/divisible_by_5_or_not.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout << " enter your number ";
+    cin >> n ;
+    if (n%5 == 0)   cout <<n << "divisible by 5";
+    else cout <<n << "not divisible by 5 ";
+}
