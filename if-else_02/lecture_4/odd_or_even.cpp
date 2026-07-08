88e013aa86536fe06967bb6f26059077a19073ef
Suraj Kumar <surajkm898@gmail.com>
2026-07-08 23:19:23 +0530

    update

---
+++ if-else_02/lecture_4/odd_or_even.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout << " enter your number ";
+    cin >> n ;
+    if (n%2 == 0)   cout <<n << "even number";
+    else cout <<n << "odd number ";
+}
