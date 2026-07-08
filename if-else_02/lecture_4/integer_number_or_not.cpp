a7d97e7927ed25200af58a97058390341a9bcd31
Suraj Kumar <surajkm898@gmail.com>
2026-07-08 23:35:44 +0530

    update

---
+++ if-else_02/lecture_4/integer_number_or_not.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    float n ;
+    cout << " enter your number ";
+    cin >> n ;
+    int x = (int)n;
+    if (x == n)   cout << "integer number";
+    else cout << "not a integer number ";
+}
