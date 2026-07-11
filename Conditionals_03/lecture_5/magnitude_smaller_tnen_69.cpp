3fe2d3bc3c6456946ce7dcffffd8a3f1fd2e2dc0
Suraj Kumar <surajkm898@gmail.com>
2026-07-11 09:32:23 +0530

    update

--- Conditionals_03/lecture_5/magnitude_smaller_tnen_69.cpp
+++ Conditionals_03/lecture_5/magnitude_smaller_tnen_69.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 int main (){
-    float n ;
+    float n , y ;
     cout << " enter your number ";
     cin >> n ;
-    if ( n >= 69)   cout << "not smaller than 69.";
+    if ( n >= 0) y = n; 
+    else  y = -n ;
+    if (y >= 69 )  cout << "not smaller than 69.";
     else cout << " smaller than 69  ";
 }
