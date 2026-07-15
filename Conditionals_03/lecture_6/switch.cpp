c51292d52ef78db89b7b30ce5d157c820e4e888c
Suraj Kumar <surajkm898@gmail.com>
2026-07-15 22:54:58 +0530

    update

---
+++ Conditionals_03/lecture_6/switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ;
+int main (){
+    int n ;
+    cout << " enter your number " ;
+    cin >> n ;
+    switch ( n){
+        case (1): cout <<" mon"; break;
+        case (2): cout <<" tus"; break;
+        case (3): cout <<" thas";break;
+        break;
+        case (4): cout <<" web";break;
+        case (5): cout <<" fri";break;
+        case (6): cout <<" sat";break;
+        case (7): cout <<" sun";break;
+    }
+}
