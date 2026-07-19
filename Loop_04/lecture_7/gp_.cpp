0629e07dcf6a60f22964c8f63ca9ba062e9c8d66
Suraj Kumar <surajkm898@gmail.com>
2026-07-19 23:35:03 +0530

    update

---
+++ Loop_04/lecture_7/gp_.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a ,d ,n;
+    cout <<" enter fist term" ;
+    cin >> a;
+    cout <<" enter com  milti" ;
+    cin >>d ;
+    cout <<" enter  number of terms" ;
+    cin >> n;
+     int j = 10;
+    for (int i = a;i<=n;i*=d){
+        cout<<i<<" ";
+    }
+}
