e78869d4c6da619062aea89298a0464a4f0c9ecb
Suraj Kumar <surajkm898@gmail.com>
2026-07-19 23:47:56 +0530

    update

---
+++ Loop_04/lecture_7/series_of-_.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a=99 ,d=4 ,n;
+    cout <<" enter  number of terms" ;
+    cin >> n;
+     int j = 10;
+    for (int i = a;i>=n;i-=d){
+        cout<<i<<" ";
+    }
+}
