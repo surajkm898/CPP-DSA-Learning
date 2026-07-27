6baa2a216e31a940935e232864ca484262a6f01e
Suraj Kumar <surajkm898@gmail.com>
2026-07-27 23:54:31 +0530

    update

---
+++ Functions_06/lecture_13/print_npr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int far(int n){
+    int a=1;
+    for (int i=1;i<=n;i++){
+        a *=i;
+    }
+        return a;
+}
+int npr(int n, int r){
+    int a = far(n)/far(n-r);
+}
+int main (){
+    cout<<npr(5 ,2);
+}
