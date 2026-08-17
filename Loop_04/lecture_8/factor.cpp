d4f93769a954323ab3b4e127bc8a1c2ebaaf6a4d
Suraj Kumar <surajkm898@gmail.com>
2026-08-17 21:55:24 +0530

    update

--- Loop_04/lecture_8/factor.cpp
+++ Loop_04/lecture_8/factor.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main (){
     int n;
     cout << " enter your numer " ;
     cin>> n;
-    for (int i=1;i<=n;i++){
+    for (int i=1;i<=sqrt(n);i++){
         if(n%i==0){
             cout<<i<<" ";
+            if(n != n/i)cout<<n/i<<" ";
         }
     }
 }
