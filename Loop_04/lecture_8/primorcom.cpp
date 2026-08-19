ebc57801953cad2ed0b21f54f78c3e1ae5b6c99d
Suraj Kumar <surajkm898@gmail.com>
2026-08-19 22:18:36 +0530

    update

---
+++ Loop_04/lecture_8/primorcom.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+    int n;
+    cout <<"enter your number ";
+    cin >>n;
+    int count = 0;
+    for (int i=1;i<=n;i++){
+        if(n%i==0){
+            count ++;
+        }
+    }
+    if (count <= 3) {
+        cout <<"com number ";
+    }else cout <<" prime number ";
+}
