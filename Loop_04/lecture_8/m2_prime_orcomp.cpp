acefbe40ab6f19e39c756ee70ac2f664f3461d9f
Suraj Kumar <surajkm898@gmail.com>
2026-08-19 22:39:21 +0530

    update

---
+++ Loop_04/lecture_8/m2_prime_orcomp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ;
+int main (){
+    int n;
+    cout <<"enter your number ";
+    cin >>n;
+    if(n == 1) cout << " nor com or not a prime ";
+    bool flag = false; //let a flase me prime
+    for (int i=2;i<=n-1;i++){
+        if(n%i==0){
+            flag =true ;
+            break;
+        }
+    }
+    if (flag==true) cout <<"com number";
+    else cout <<" prime number ";
+}
