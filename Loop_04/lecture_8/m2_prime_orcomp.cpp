6bc9cb0928d0c94b238210cfb527416411a43056
Suraj Kumar <surajkm898@gmail.com>
2026-08-20 22:15:42 +0530

    update

--- Loop_04/lecture_8/m2_prime_orcomp.cpp
+++ Loop_04/lecture_8/m2_prime_orcomp.cpp
@@ -3,8 +3,7 @@ using namespace std ;
 int main (){
     int n;
     cout <<"enter your number ";
-    cin >>n;
-    if(n == 1) cout << " nor com or not a prime ";
+ cin >>n;   
     bool flag = false; //let a flase me prime
     for (int i=2;i<=n-1;i++){
         if(n%i==0){
@@ -12,6 +11,7 @@ int main (){
             // break;
         }
     }
-    if (flag==true) cout <<"com number";
+     if(n == 1) cout << " nor com or not a prime ";
+      else if (flag==true) cout <<"com number";
     else cout <<" prime number ";
 }
