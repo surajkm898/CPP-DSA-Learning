9b4fc62d4f583343001e029d0ba4b7f008a16aa0
Suraj Kumar <surajkm898@gmail.com>
2026-08-29 14:21:07 +0530

    update

--- pattern_05/lecture_10/alphabet_square_sm_or_big_.cpp
+++ pattern_05/lecture_10/alphabet_square_sm_or_big_.cpp
@@ -6,10 +6,10 @@ int main (){
     cin>> n;
     for (int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(j%2!=0){
-                cout <<(char)(j+96)<<" ";
+            if(i%2!=0){
+                cout <<(char)(i+96)<<" ";
             }else{
-                cout<<(char)(j+64)<<" ";
+                cout<<(char)(i+64)<<" ";
             }
         }
         cout << endl;
