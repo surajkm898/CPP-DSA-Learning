64b121af156418676cf17278b4a9dfa46e4306de
Suraj Kumar <surajkm898@gmail.com>
2026-09-02 11:18:16 +0530

    update

--- pattern_05/lecture_10/star_plus.cpp
+++ pattern_05/lecture_10/star_plus.cpp
@@ -6,8 +6,8 @@ int main (){
     cin>>n;
     for (int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-             if(j==((n/2)+1)||i==((n/2)+1)){cout<<"*";
-                        }else cout<<" ";
+             if(j==((n/2)+1)||i==((n/2)+1)){cout<<"*"<<" ";
+                        }else cout<<" "<<" ";
         }
         cout<<endl;
     }
