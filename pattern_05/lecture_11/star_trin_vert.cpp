f4a097c46c12b6a45e239d02f718cbf279a2feb7
Suraj Kumar <surajkm898@gmail.com>
2026-09-04 16:00:55 +0530

    update

---
+++ pattern_05/lecture_11/star_trin_vert.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout <<"enter your number ";
+    cin>>n;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i+j>n){
+                    cout<<" * ";
+                }else cout<<" # ";
+            }
+            cout<< endl;
+        }
+}
