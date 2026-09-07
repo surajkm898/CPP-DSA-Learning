4886070a084a7c93f65e7b6b4ee7778163800ac4
Suraj Kumar <surajkm898@gmail.com>
2026-09-07 22:50:50 +0530

    update

--- pointer_07/lecture_15/tempCodeRunnerFile.cpp
+++ pointer_07/lecture_15/tempCodeRunnerFile.cpp
@@ -1,3 +1,12 @@
-
+#include <iostream>
+using namespace std;
+int sum(int* aptm, int* bptm){
+    int c = *aptm+*bptm ;
+    return c;
+}
+int main (){
+   int a, b;
+   cout << "enter your a and b number";
+   cin>>a>>b;
    cout << sum(&a,&b);
 }
