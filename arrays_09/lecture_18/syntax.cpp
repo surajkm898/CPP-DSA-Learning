51d278703ff55e5a87a274835c9c5ecb105ceff0
Suraj Kumar <surajkm898@gmail.com>
2026-09-08 22:17:00 +0530

    update

---
+++ arrays_09/lecture_18/syntax.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int x[] = {23,4,55,46,23,78};
+    cout <<x[2]<<endl;
+      x[2] = 10;
+    cout <<x[2]<<endl;
+    cin >>x[2];
+    cout <<x[2]<<endl;
+}
