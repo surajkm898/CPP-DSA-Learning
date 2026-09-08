53a140a15ef2ac841187fce35e8021de3ee09e79
Suraj Kumar <surajkm898@gmail.com>
2026-09-08 22:30:19 +0530

    update

--- arrays_09/lecture_18/syntax.cpp
+++ arrays_09/lecture_18/syntax.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <iterator>
 using namespace std;
 int main (){
     int x[] = {23,4,55,46,23,78};
+    //cout<<std::size(x)<<endl; for new c++
+    cout <<sizeof(x)/4<<endl;
     cout <<x[2]<<endl;
       x[2] = 10;
     cout <<x[2]<<endl;
