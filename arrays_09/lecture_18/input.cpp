0f4702a266abbde95b347656cb3436c9173dc36c
Suraj Kumar <surajkm898@gmail.com>
2026-09-09 21:15:50 +0530

    update

---
+++ arrays_09/lecture_18/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout <<" Enter your Arrays Size : ";
+    cin >>n;
+    int x[n];
+    for(int i=0;i<=n-1;i++){
+        cin>>x[i];
+    }
+}
